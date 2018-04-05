7ecf5fd4db6b0760ba73dc26bd2040a93463ba1e
jnims <jmnims@gmail.com>
2018-04-05 00:25:35 -0400

    Doing arg stuff and things. arg.

--- door_controller.py
+++ door_controller.py
@@ -1,5 +1,16 @@
-import configparser as config
+import argparse
+import configparser
 import door_handler as door
 import paho.mqtt.client as mqtt
 
+VERSION = '0.1a'
 
+# Get the config file name
+parser = argparse.ArgumentParser(description='File name of the config file used to launch the daemon.',
+                                 epilog='Quantum states not supported.',
+                                 prog='door_controller.py')
+parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+parser.add_argument('--config', type=str, default='example.config',
+                    help='Relative path to the config file for the daemon instance')
+
+args = parser.parse_args()
