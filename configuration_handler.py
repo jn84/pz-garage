5944b24af6802221f79303295ee0fbda9848699c
jnims <jmnims@gmail.com>
2018-04-05 15:59:15 -0400

    Finished implementing argument parsing for config file Implemented ConfigurationHandler class to parse configuration and store the information contained within

---
+++ configuration_handler.py
@@ -0,0 +1,141 @@
+import configparser
+
+from random import choice
+
+
+class ConfigurationHandler:
+    MQTT_HOST = None  # type: str
+    MQTT_PORT = None  # type: int
+    MQTT_CLIENT_ID = None  # type: str
+    MQTT_USE_AUTHENTICATION = None  # type: bool
+    MQTT_USERNAME = None  # type: str
+    MQTT_PASSWORD = None  # type: str
+    MQTT_USE_SSL = None  # type: bool
+    MQTT_PORT_SSL = None  # type: int
+    MQTT_TOPIC_TOGGLE_DOOR_STATE = None  # type: str
+    MQTT_TOPIC_REPORT_DOOR_STATE = None  # type: str
+    DOOR_CONTROL_OUTPUT_PIN = None  # type: int
+    DOOR_OPEN_SENSOR_INPUT_PIN = None  # type: int
+    DOOR_CLOSED_SENSOR_INPUT_PIN = None  # type: int
+    DOOR_CONTROL_OUTPUT_ACTIVE_VALUE = None  # type: bool
+    DOOR_CONTROL_OUTPUT_ACTIVE_DELAY = None  # type: bool
+    DOOR_OPEN_SENSOR_INPUT_ACTIVE_VALUE = None  # type: bool
+    DOOR_CLOSED_SENSOR_ACTIVE_VALUE = None  # type: bool
+
+    _hexdigits = '0123456789abcdef'
+
+    def generate_client_id(self):
+        self.MQTT_CLIENT_ID = 'door_' + ''.join(choice(self._hexdigits) for _ in range(32))
+
+    # Config parsing helpers
+    @staticmethod
+    def bool_parse(str_value: str, var_name: str, default: bool) -> object:
+        val = str.lower(str_value)
+        if val == '':
+            return default
+        if val == 'true' or val == 'high' or val == '1':
+            return True
+        if val == 'false' or val == 'low' or val == '0':
+            return False
+        raise TypeError('Config value "' + str_value + '" is invalid for ' + var_name)
+
+    @staticmethod
+    def int_parse(str_value: str, var_name: str, can_be_none: bool) -> object:
+        if (str_value == '' or str_value is None) and can_be_none:
+            return None
+        if (str_value == '' or str_value is None) and not can_be_none:
+            raise ValueError("Config value cannot be empty for " + var_name)
+        return int(str_value)
+
+    @staticmethod
+    def float_parse(str_value: str, var_name: str, can_be_none: bool) -> object:
+        if (str_value == '' or str_value is None) and can_be_none:
+            return None
+        if (str_value == '' or str_value is None) and not can_be_none:
+            raise ValueError("Config value cannot be empty for " + var_name)
+        return float(str_value)
+
+    @staticmethod
+    def str_parse(str_value: str, var_name: str, can_be_none: bool) -> object:
+        if str_value == '' and can_be_none:
+            return None
+        if str_value == '' and not can_be_none:
+            raise TypeError('Config value is invalid for ' + var_name + ': value cannot be empty')
+        return str(str_value)
+
+    def __init__(self, config_file):
+
+        config = configparser.ConfigParser()
+        config.read(config_file)
+
+        self.MQTT_HOST = self.\
+            str_parse(config['MQTTBrokerConfig']['mqtt_host'],
+                      'mqtt_host',
+                      False)
+        self.MQTT_PORT = self.\
+            int_parse(config['MQTTBrokerConfig']['mqtt_port'],
+                      'mqtt_port',
+                      False)
+        self.MQTT_CLIENT_ID = self.\
+            str_parse(config['MQTTBrokerConfig']['mqtt_client_id'],
+                      'mqtt_client_id',
+                      True)
+        self.MQTT_USE_AUTHENTICATION = self.\
+            bool_parse(config['MQTTBrokerConfig']['mqtt_use_authentication'],
+                       'mqtt_use_authentication',
+                       False)
+        self.MQTT_USERNAME = self.\
+            str_parse(config['MQTTBrokerConfig']['mqtt_username'],
+                      'mqtt_username',
+                      not self.MQTT_USE_AUTHENTICATION)
+        self.MQTT_PASSWORD = self.\
+            str_parse(config['MQTTBrokerConfig']['mqtt_password'],
+                      'mqtt_password',
+                      not self.MQTT_USE_AUTHENTICATION)
+        self.MQTT_USE_SSL = self.\
+            bool_parse(config['MQTTBrokerConfig']['mqtt_use_ssl'],
+                       'mqtt_use_ssl',
+                       False)
+        self.MQTT_PORT_SSL = self.\
+            int_parse(config['MQTTBrokerConfig']['mqtt_port_ssl'],
+                      'mqtt_port_ssl',
+                      not self.MQTT_USE_SSL)
+        self.MQTT_TOPIC_TOGGLE_DOOR_STATE = self.\
+            str_parse(config['MQTTTopicConfig']['mqtt_topic_toggle_door_state'],
+                      'mqtt_topic_toggle_door_state',
+                      False)
+        self.MQTT_TOPIC_REPORT_DOOR_STATE = self.\
+            str_parse(config['MQTTTopicConfig']['mqtt_topic_report_door_state'],
+                      'mqtt_topic_report_door_state',
+                      False)
+        self.DOOR_CONTROL_OUTPUT_PIN = self.\
+            int_parse(config['GPIOConfig']['door_control_output_pin'],
+                      'door_control_output_pin',
+                      False)
+        self.DOOR_OPEN_SENSOR_INPUT_PIN = self.\
+            int_parse(config['GPIOConfig']['door_open_sensor_input_pin'],
+                      'door_open_sensor_input_pin',
+                      False)
+        self.DOOR_CLOSED_SENSOR_INPUT_PIN = self.\
+            int_parse(config['GPIOConfig']['door_closed_sensor_input_pin'],
+                      'door_closed_sensor_input_pin',
+                      False)
+        self.DOOR_CONTROL_OUTPUT_ACTIVE_VALUE = self.\
+            bool_parse(config['GPIOActiveStates']['door_control_output_active_value'],
+                       'door_control_output_active_value',
+                       True)
+        self.DOOR_CONTROL_OUTPUT_ACTIVE_DELAY = self.\
+            float_parse(config['GPIOActiveStates']['door_control_output_active_delay'],
+                        'door_control_output_active_delay',
+                        False)
+        self.DOOR_OPEN_SENSOR_INPUT_ACTIVE_VALUE = self.\
+            bool_parse(config['GPIOActiveStates']['door_open_sensor_input_active_value'],
+                       'door_open_sensor_input_active_value',
+                       True)
+        self.DOOR_CLOSED_SENSOR_ACTIVE_VALUE = self.\
+            bool_parse(config['GPIOActiveStates']['door_closed_sensor_active_value'],
+                       'door_closed_sensor_active_value',
+                       True)
+
+        if self.MQTT_CLIENT_ID == '' or self.MQTT_CLIENT_ID is None:
+            self.generate_client_id()
--- door_controller.py
+++ door_controller.py
@@ -1,16 +1,48 @@
 import argparse
-import configparser
-import door_handler as door
+import configuration_handler
+import door_handler
+import string
+import sys
 import paho.mqtt.client as mqtt
+
+from os import path
+from os import access
+from os import R_OK
+from configparser import Error as ConfigError
 
 VERSION = '0.1a'
 
 # Get the config file name
-parser = argparse.ArgumentParser(description='File name of the config file used to launch the daemon.',
+parser = argparse.ArgumentParser(description='Control a door. Probably a garage  door.',
                                  epilog='Quantum states not supported.',
                                  prog='door_controller.py')
-parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
-parser.add_argument('--config', type=str, default='example.config',
-                    help='Relative path to the config file for the daemon instance')
+parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + VERSION)
+parser.add_argument('-c', '--config', type=str, default='example.config', dest='config_file',
+                    help='File name of the config file used to launch the daemon.'
+                         'If in the local directory (./) input only the file name.'
+                         'If in another directory, input the full path.                 '
+                         'The program MUST be run using a config file. If one is not '
+                         'specified, the default example.config will be used. Make a '
+                         'copy of example.config and modify the settings to suit your needs.')
 
+# Get the config file name
 args = parser.parse_args()
+
+if not path.isfile(args.config_file) or not access(args.config_file, R_OK):
+    print('The configuration file could not be found or read access was denied by the system.')
+    sys.exit('Unable to access config file: ' + args.config_file)
+
+# Parse the config file
+
+try:
+    config = configuration_handler.ConfigurationHandler(args.config_file)
+except ConfigError as e:
+    print(str(e))
+    sys.exit("Error parsing the config file")
+except TypeError as e:
+    print(str(e))
+    sys.exit("Error converting config variable to correct type. Check that all configuration variables "
+             "are in the correct format.")
+
+print(config.MQTT_CLIENT_ID)
+
